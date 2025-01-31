a4101adc68dfd641fc5d1d9c6f5ce973fa58036c
yrlanrteixeira <yrlan.01@hotmail.com>
2025-01-30 22:57:07 -0300

    feat: Implement question and answer attachment entities and update create question use case to handle attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
 import { CreateQuestionUseCase } from './create-question';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: CreateQuestionUseCase;
@@ -15,9 +16,18 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Nova pergunta',
       content: 'Conteúdo da pergunta',
+      attachmentsIds: ['1', '2'],
     });
 
     expect(result.isRight()).toBeTruthy();
-    expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    ]);
   });
 });
--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { Question } from '@/domain/forum/enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/question-repository';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Either, right } from '@/core/either';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type CreateQuestionUseCaseResponse = Either<null, { question: Question }>;
@@ -14,12 +16,26 @@ type CreateQuestionUseCaseResponse = Either<null, { question: Question }>;
 export class CreateQuestionUseCase {
   constructor(private questionRepository: QuestionsRepository) {}
 
-  async execute({ authorId, title, content }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
+  async execute({
+    authorId,
+    title,
+    content,
+    attachmentsIds,
+  }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       title,
       content,
     });
+
+    const questionAttachment = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      });
+    });
+
+    question.attachments = questionAttachment;
 
     await this.questionRepository.create(question);
 
